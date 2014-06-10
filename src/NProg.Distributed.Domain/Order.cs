0528025fd07c657fd3068c17f0cb11267e0b2722
Jacek Spolnik <jacek.spolnik@gmail.com>
2014-06-10 22:36:10 +0200

    - add missing files

---
+++ src/NProg.Distributed.Domain/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NProg.Distributed.Domain
+{
+    public class Order
+    {
+        public Guid OrderId { get; set; }
+
+        public int Count { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public string UserName { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("OrderId: {0}, Count: {1}, OrderDate: {2}, UnitPrice: {3}, UserName: {4}", OrderId,
+                Count, OrderDate, UnitPrice, UserName);
+        }
+    }
+}
