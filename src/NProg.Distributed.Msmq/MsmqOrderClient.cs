22a144dbc470be01b39fa4827288e1878a652f9c
Jacek Spolnik <jacek.spolnik@gmail.com>
2014-06-13 18:51:44 +0200

    - fix msmq implementation

--- src/NProg.Distributed.Msmq/MsmqOrderClient.cs
+++ src/NProg.Distributed.Msmq/MsmqOrderClient.cs
@@ -11,51 +11,65 @@ namespace NProg.Distributed.Msmq
     {
         public void Add(Order item)
         {
-            var messageQueue = MessageQueueFactory.CreateOutbound(AddOrderRequest.Name, MessagePattern.RequestResponse);
-            messageQueue.Send(new Message
+            using (var requestQueue = MessageQueueFactory.CreateOutbound(AddOrderRequest.Name, MessagePattern.RequestResponse))
             {
-                Body = new AddOrderRequest { Order = item }
-            });
+                using (var responseQueue = requestQueue.GetResponseQueue())
+                {
+                    requestQueue.Send(new Message
+                    {
+                        Body = new AddOrderRequest { Order = item },
+                        ResponseAddress = responseQueue.Address
+                    });
 
-            var responseQueue = messageQueue.GetResponseQueue();
-
-            responseQueue.Receive(x => Console.WriteLine("Order added: {0}", x.BodyAs<StatusResponse>().Status));
+                    responseQueue.Receive(x => Console.WriteLine("Order added: {0}", x.BodyAs<StatusResponse>().Status));
+                }
+            }
         }
 
         public Order Get(Guid guid)
         {
-            var messageQueue = MessageQueueFactory.CreateOutbound(GetOrderRequest.Name, MessagePattern.RequestResponse);
-            messageQueue.Send(new Message
-            {
-                Body = new GetOrderRequest { OrderId = guid }
-            });
-
-            var responseQueue = messageQueue.GetResponseQueue();
-
             Order order = null;
-            responseQueue.Receive(x =>
+
+            using (var requestQueue = MessageQueueFactory.CreateOutbound(GetOrderRequest.Name, MessagePattern.RequestResponse))
             {
-                order = x.BodyAs<GetOrderResponse>().Order;
-            });
+                using (var responseQueue = requestQueue.GetResponseQueue())
+                {
+                    requestQueue.Send(new Message
+                    {
+                        Body = new GetOrderRequest { OrderId = guid },
+                        ResponseAddress = responseQueue.Address
+                    });
+                    
+                    responseQueue.Receive(x =>
+                    {
+                        order = x.BodyAs<GetOrderResponse>().Order;
+                    });
+                }
+            }
 
             return order;
         }
 
         public bool Remove(Guid guid)
         {
-            var messageQueue = MessageQueueFactory.CreateOutbound(RemoveOrderRequest.Name, MessagePattern.RequestResponse);
-            messageQueue.Send(new Message
-            {
-                Body = new RemoveOrderRequest { OrderId = guid }
-            });
-
-            var responseQueue = messageQueue.GetResponseQueue();
-
             var status = false;
-            responseQueue.Receive(x =>
+
+            using (var requestQueue = MessageQueueFactory.CreateOutbound(RemoveOrderRequest.Name, MessagePattern.RequestResponse))
             {
-                status = x.BodyAs<StatusResponse>().Status;
-            });
+                using (var responseQueue = requestQueue.GetResponseQueue())
+                {
+                    requestQueue.Send(new Message
+                    {
+                        Body = new RemoveOrderRequest { OrderId = guid },
+                        ResponseAddress = responseQueue.Address
+                    });
+
+                    responseQueue.Receive(x =>
+                    {
+                        status = x.BodyAs<StatusResponse>().Status;
+                    });
+                }
+            }
 
             return status;
         }
