534c55bc25684717e3b3974ffa6971a692662874
Jacek Spolnik <jacek.spolnik@gmail.com>
2014-06-19 17:40:24 +0200

    - add logical service

---
+++ src/NProg.Distributed.Domain/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NProg.Distributed.Domain
+{
+    public interface IOrderService
+    {
+        void Add(Guid key, Order value);
+
+        Order Get(Guid key);
+
+        bool Remove(Guid key);
+    }
+}
